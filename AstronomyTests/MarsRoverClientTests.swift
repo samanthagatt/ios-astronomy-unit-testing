b0a5c2be8ce33c46d7564a8dd4b0bd7d27183441
Samantha Gatt <smfgatt@gmail.com>
2018-09-17 15:17:19 -0400

    Refactors MarsRoverClient for dependency injection

---
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  AstronomyTests.swift
+//  AstronomyTests
+//
+//  Created by Samantha Gatt on 9/17/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class AstronomyTests: XCTestCase {
+    
+    func testFetch() {
+        
+    }
+    
+}
