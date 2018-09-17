4f21493ae54df7bd40d12408a0b6632c1b21b9c9
Samantha Gatt <smfgatt@gmail.com>
2018-09-17 16:42:35 -0400

    Creates unit test for fetchPhotos function when it recieves an error

--- AstronomyTests/MarsRoverClientTests.swift
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -18,18 +18,18 @@ class MockDataLoader: NetworkDataLoader {
     
     func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
         self.url = url
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+        DispatchQueue.global().async {
             completion(self.data, self.error)
         }
     }
     
     func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
         self.request = request
-        // runs code inside closure one second in the future
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+        DispatchQueue.global().async {
             completion(self.data, self.error)
         }
     }
+    
     let data: Data?
     let error: Error?
     private(set) var request: URLRequest? = nil
@@ -59,18 +59,16 @@ func urlComponents(_ componenst1: URLComponents, equalTo components2: URLCompone
 
 class AstronomyTests: XCTestCase {
     
+    let testMarsRover = MarsRover(name: "Curiosity", launchDate: Date(), landingDate: Date(), status: .active, maxSol: 3, maxDate: Date(), numberOfPhotos: 90, solDescriptions: [SolDescription(sol: 4, totalPhotos: 9, cameras: ["Hi"])])
+    
     func testFetchPhotosWithValidData() {
         
         let expectation = self.expectation(description: "Fetch photos")
         
-        let solDescriptions = SolDescription(sol: 4, totalPhotos: 9, cameras: ["Hi"])
-        let marsRover = MarsRover(name: "Curiosity", launchDate: Date(), landingDate: Date(), status: .active, maxSol: 3, maxDate: Date(), numberOfPhotos: 90, solDescriptions: [solDescriptions])
-        
-        
         let mock = MockDataLoader(data: testJSON)
         let marsRoverClient = MarsRoverClient(dataLoader: mock)
         
-        marsRoverClient.fetchPhotos(from: marsRover, onSol: 0) { (photoRefs, error) in
+        marsRoverClient.fetchPhotos(from: testMarsRover, onSol: 0) { (photoRefs, error) in
             
             XCTAssertNotNil(mock.url)
             let components = URLComponents(url: mock.url!, resolvingAgainstBaseURL: true)!
@@ -83,6 +81,23 @@ class AstronomyTests: XCTestCase {
             let firstObject = photoRefs!.first!
             XCTAssertEqual(firstObject.id, 727)
             
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    func testFetchPhotosWithError() {
+        let expectation = self.expectation(description: "Fetch photos")
+        
+        let mock = MockDataLoader(error: error)
+        let marsRoverClient = MarsRoverClient(dataLoader: mock)
+        
+        let error = NSError(domain: "Error not found", code: 404, userInfo: nil)
+        
+        marsRoverClient.fetchPhotos(from: testMarsRover, onSol: 0) { (photoRefs, error) in
+            XCTAssertNil(photoRefs)
+            XCTAssertNotNil(error)
             expectation.fulfill()
         }
         
